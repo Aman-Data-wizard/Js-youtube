14ebfd207fb8fa225e72280204d0b537a77c8e15
Aman <singh.mie4166@gmail.com>
2026-02-25 12:59:45 +0000

    initial update

--- 01_basics/Datatypes_summary.js
+++ 01_basics/Datatypes_summary.js
@@ -5,8 +5,33 @@
 // null, 
 // undefined, 
 // symbol
+const score = 100;
+const scoreValue = 100.5;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; //undefined
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId) //false because symbol is unique
+
+const bigNumber = 9007199254740991n; //bigint
+
+console.log(typeof bigNumber)
+
 
 //Non-primitive datatypes (Reference datatypes):
 // object, Master -> Web Events -> Browser Events -> DOM Events -> Mouse Events -> click, dblclick, mousedown, mouseup, mousemove, mouseover, mouseout, mouseenter, mouseleave
 // array, 
 // function
+
+const heros = ["Spiderman", "Ironman", "Thor"]; //Array is a type of object
+let myObj = {
+    name: "Bruce",
+    age:22,
+    isAvenger:false
+}
+console.log(typeof heros) //object
+
+//check: https://262.ecma-international.org/5.1/#sec-11.4.3
