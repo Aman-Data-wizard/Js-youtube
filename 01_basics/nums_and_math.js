f4a8cb764b24a55dc877bbbb24d666816cf72376
Aman <singh.mie4166@gmail.com>
2026-03-09 12:44:56 +0000

    New Update

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400;
+
+const balance = new Number(1000);
+
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.456789;
+console.log(otherNumber. toPrecision(3))
+
+const num1 = 100000;
+console.log(num1.toLocaleString('en-IN'));
+
+/* Maths Library 
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.3));
+*/
+console.log(Math.random())
+console.log((Math.random()*10) + 1);
+console.log((Math.floor(Math.random()*10) + 1));
+
+const Min =10;
+const Max = 20;
+
+console.log(Math.floor(Math.random() * (Max - Min + 1) + Min));
+
